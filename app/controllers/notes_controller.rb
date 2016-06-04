df45285171ca488aedd343e166387859ed070ac9
Joe Spampinato <jcspampinato@gmail.com>
2016-06-04 16:04:25 -0400

    Generated notes controller and added routes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,29 @@
+class NotesController < ApplicationController
+
+	def create
+	end
+
+	def destroy
+	end
+
+	def edit
+	end
+
+	def new
+	end
+
+	def show
+	end
+
+	def update
+	end
+
+	private
+
+	def find_note
+	end
+
+	def note_params
+	end
+
+end
